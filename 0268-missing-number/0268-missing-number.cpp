d833c9911890775f985e22c5738a20e5c1e192a5
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2024-11-25 22:08:11 +0530

    Time: 3 ms (22.18%), Space: 22 MB (5.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        std :: vector<int> FlagVec(n+1 , 0);
+        std :: sort(nums.begin() , nums.end());
+        for(int i : nums){
+            FlagVec[i] = 1;
+        }
+        int missing = 0;
+        for(int j = 0 ; j <= n ; j++){
+            if(FlagVec[j] == 0){
+                missing = j;
+            }
+        }
+        return missing;
+    }
+};
