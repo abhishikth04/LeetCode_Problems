90591875c6d65b7cc6d65fac565074c38323c001
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2025-01-02 21:07:46 +0530

    Time: 0 ms (100%), Space: 13.5 MB (5.07%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        std :: sort(citations.rbegin() , citations.rend());
+
+        for(int i {0} ; i < n ; i++){
+            if(citations[i] >= i+1){
+                continue;
+            }else{
+                return i;
+            }
+        }
+        return n;
+
+    }
+};
