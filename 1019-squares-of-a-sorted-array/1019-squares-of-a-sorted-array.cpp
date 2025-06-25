58c709415193951499a2739d3b75cec57151461d
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2025-06-25 23:36:47 +0530

    Time: 3 ms (54.61%), Space: 30.3 MB (37.99%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -2,10 +2,25 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            nums[i] = nums[i]*nums[i];
+        vector<int> ans(n);
+
+        int left = 0;
+        int right = n-1;
+        int posi = n-1;
+
+        while(left <= right){
+            int leftsq = nums[left]*nums[left];
+            int rightsq = nums[right]*nums[right];
+
+            if(leftsq > rightsq){
+                ans[posi] = leftsq;
+                left++;
+            } else{
+                ans[posi] = rightsq;
+                right--;
+            }
+            posi--;
         }
-        std::sort(nums.begin() , nums.end());
-        return nums;
+        return ans;
     }
 };
