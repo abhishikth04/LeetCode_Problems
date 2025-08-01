f4fa7ace0a7c813d33540ca2491c0da0118b365e
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2025-08-01 20:19:26 +0530

    Time: 0 ms (100%), Space: 25.6 MB (92.16%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -2,25 +2,49 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
-        int m = n*n;
-        vector<int> flags(m+1 , 0);
-        int a;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(flags[grid[i][j]] == 1){
-                    a = grid[i][j];
-                } else{
-                    flags[grid[i][j]] = 1;
-                }
-            }
-        }
-        int b;
-        for(int i = 1 ; i < m+1 ; i++){
-            if(flags[i] == 0){
-                b = i;
+        int m = n * n;
+
+        long long sum_actual = 0, sum_sq_actual = 0;
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < n ; j++) {
+                int val = grid[i][j];
+                sum_actual += val;
+                sum_sq_actual += 1LL * val * val;
             }
         }
 
-        return {a , b};
+        long long sum_ideal = (1LL * m * (m + 1)) / 2;
+        long long sum_sq_ideal = (1LL * m * (m + 1) * (2 * m + 1)) / 6;
+
+        long long S = sum_actual - sum_ideal;
+        long long P = sum_sq_actual - sum_sq_ideal;
+
+        long long x_plus_y = P / S;
+        int x = (S + x_plus_y) / 2;
+        int y = x - S;
+
+        return {x, y}; // x = repeated, y = missing
     }
 };
+
+// int n = grid.size();
+//         int m = n*n;
+//         vector<int> flags(m+1 , 0);
+//         int a;
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = 0 ; j < n ; j++){
+//                 if(flags[grid[i][j]] == 1){
+//                     a = grid[i][j];
+//                 } else{
+//                     flags[grid[i][j]] = 1;
+//                 }
+//             }
+//         }
+//         int b;
+//         for(int i = 1 ; i < m+1 ; i++){
+//             if(flags[i] == 0){
+//                 b = i;
+//             }
+//         }
+
+//         return {a , b};
