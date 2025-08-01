242eadb289389f6609a35024b95038f3d004a42d
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2025-08-01 19:31:01 +0530

    Time: 0 ms (100%), Space: 27.9 MB (98.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,10 +2,30 @@ class Solution {
 public:
 // sol : majority element appears more than n / 2 times
 // so the majority element should be present at n/2 th position
-    int majorityElement(vector<int>& nums) {
-        std :: sort(nums.begin() , nums.end());
-        int x = nums.size() / 2;
-        return nums[x];
+     int majorityElement(vector<int>& nums) {
+
+        int count = 0;
+        int candidate = 0;
+
+        for(int num : nums) {
+            if(count == 0)
+                candidate = num;
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+    // Step 2: Verify candidate
+        count = 0;
+        for(int num : nums) {
+            if(num == candidate)
+                count++;
+        }
+
+        return (count > nums.size() / 2) ? candidate : -1;
+
+        // std :: sort(nums.begin() , nums.end());
+        // int x = nums.size() / 2;
+        // return nums[x];
         
     }
 };
