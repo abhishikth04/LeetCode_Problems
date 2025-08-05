1a951df376983767c0b949211f342537be46704e
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2025-08-05 22:03:00 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.64%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+
+        // If Negative, turn it intop fraction;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+
+        double res = 1;
+        while(N > 0){
+            if(N % 2 == 1){
+                res *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return res;
+
+    }
+};
