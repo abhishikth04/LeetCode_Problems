28fcdccd6c0b3929c5379ec90570d980aaa5004b
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2026-04-18 15:32:33 +0530

    Time: 3 ms (80.96%), Space: 11.9 MB (56.28%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> window;
-        int left = 0;
-        int max_len = INT_MIN;
-        int n = s.size();
 
-        for(int right = 0 ; right < n ; right++){
-            while(window.find(s[right]) != window.end()){ 
-                window.erase(s[left]);
-                left++;
-            }
-            window.insert(s[right]);
-            max_len = std::max(max_len , right-left+1);
+        unordered_map<char, int> mp;
+
+    int left = 0;
+    int maxLen = 0;
+
+    for(int right = 0; right < s.size(); right++) {
+        char ch = s[right];
+
+        if(mp.find(ch) != mp.end()) {
+            left = max(left, mp[ch] + 1);
         }
-        return (max_len == INT_MIN) ? 0 : max_len;
+
+        mp[ch] = right;
+
+        maxLen = max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+
+
+        // unordered_set<char> window;
+        // int left = 0;
+        // int max_len = INT_MIN;
+        // int n = s.size();
+
+        // for(int right = 0 ; right < n ; right++){
+        //     while(window.find(s[right]) != window.end()){ 
+        //         window.erase(s[left]);
+        //         left++;
+        //     }
+        //     window.insert(s[right]);
+        //     max_len = std::max(max_len , right-left+1);
+        // }
+        // return (max_len == INT_MIN) ? 0 : max_len;
     }
 };
