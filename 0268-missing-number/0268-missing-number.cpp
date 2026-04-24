e5ba1ef7c9ece22b87e3dc92370cde4be171d056
abhishikth04 <155514630+abhishikth04@users.noreply.github.com>
2026-04-24 16:35:36 +0530

    Time: 0 ms (100%), Space: 21.6 MB (99.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,19 +2,10 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        std :: vector<int> FlagVec(n+1 , 0);
-        std :: sort(nums.begin() , nums.end());
-        for(int i : nums){
-            FlagVec[i] = 1;
-        }
-        int missing = 0;
-        for(int j = 0 ; j <= n ; j++){
-            if(FlagVec[j] == 0){
-                missing = j;
-            }
+        int totSum = (n*(n+1)) / 2;
 
-            // you could also solve by subracting arraysum - n(n-1)/2
-        }
-        return missing;
+        int NumSum = std :: accumulate(nums.begin() , nums.end() , 0);
+
+        return totSum - NumSum;
     }
 };
